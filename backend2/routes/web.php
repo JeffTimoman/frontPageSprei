43a7a764756eea987b175fdec39044f14672107d
JeffTimoman <ppti.jeffersontimotius@gmail.com>
2024-06-30 00:30:21 +0700

    kk

--- backend2/routes/web.php
+++ backend2/routes/web.php
@@ -8,7 +8,7 @@ use App\Models\Transaction;
 use App\Models\User;
 use App\Models\WebVariable;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -30,7 +30,7 @@ Route::get('/login', function () {
 Route::post('/login', function (Request $request) {
     $credentials = $request->only('email', 'password');
     $allow_login = WebVariable::where('name', 'AllowLogin')->first();
-    if (auth()->attempt($credentials) ) {
+    if (auth()->attempt($credentials)) {
         $request->session()->regenerate();
         if (auth()->user()->role != 'admin' && $allow_login->value == '0') {
             auth()->logout();
@@ -52,32 +52,40 @@ Route::get('/logout', function () {
     return redirect()->route('user.login')->with('success', 'Logout success');
 })->name('user.logout')->middleware(isLogin::class);
 
-Route::post('/claim', function(Request $request) {
+Route::post('/claim', function (Request $request) {
     $productDepartement = ProductDepartement::findOrFail($request->id);
 
     $block_claim_time = WebVariable::where('name', 'BlockClaimTime')->first();
-    $current_time = Carbon::now();
+    if ($block_claim_time && $block_claim_time->value) {
+        // Set the current time to the desired timezone (Asia/Jakarta for Western Indonesia Time)
+        $current_time = Carbon::now('Asia/Jakarta');
 
-    if (Carbon::parse($block_claim_time->value)->lessThan($current_time)) {
-        return redirect(route('user.index').'/#claimNo'.$request->id)->with('error', 'Claim failed due to time running out.');
+        // Parse the block claim time value in the same timezone
+        $block_claim_time_parsed = Carbon::createFromFormat('Y-m-d H:i:s', $block_claim_time->value, 'Asia/Jakarta');
+
+        // Compare the parsed block claim time with the current time
+        if ($block_claim_time_parsed->lessThan($current_time)) {
+            return redirect(route('user.index') . '/#claimNo' . $request->id)->with('error', 'Claim failed due to time running out.');
+        }
+
+        // Proceed with your logic if the current time has not passed the BlockClaimTime
     }
-
-    if(auth()->user()->buying_limit <= 0) {
-        return redirect(route('user.index').'/#claimNo'.$request->id)->with('error', 'Claim failed because you have claimed two products.');
+    if (auth()->user()->buying_limit <= 0) {
+        return redirect(route('user.index') . '/#claimNo' . $request->id)->with('error', 'Claim failed because you have claimed two products.');
     }
 
     $departement = auth()->user()->departement;
     if ($productDepartement->departement_id != $departement->id) {
-        return redirect(route('user.index').'/#claimNo'.$request->id)->with('error', 'Claim failed');
+        return redirect(route('user.index') . '/#claimNo' . $request->id)->with('error', 'Claim failed');
     }
 
     $transactions = Transaction::where('product_departement_id', $productDepartement->id)->get();
     if ($productDepartement->quantity <= $transactions->count()) {
-        return redirect(route('user.index').'/#claimNo'.$request->id)->with('error', 'Claim failed because the product is out of stock');
+        return redirect(route('user.index') . '/#claimNo' . $request->id)->with('error', 'Claim failed because the product is out of stock');
     }
 
     //transactions of all user in this departement
-    $transactions = Transaction::whereHas('user', function($query) use ($departement){
+    $transactions = Transaction::whereHas('user', function ($query) use ($departement) {
         $query->where('departement_id', $departement->id);
     })->get();
     $quantity_of_all_product = ProductDepartement::where('departement_id', $departement->id)->sum('quantity');
@@ -85,8 +93,8 @@ Route::post('/claim', function(Request $request) {
 
     $check = Transaction::where('product_departement_id', $productDepartement->id)->where('user_id', auth()->id())->first();
 
-    if ($check && $check2 != 1){
-        return redirect(route('user.index').'/#claimNo'.$request->id)->with('error', 'Claim failed because you have claimed this product.');
+    if ($check && $check2 != 1) {
+        return redirect(route('user.index') . '/#claimNo' . $request->id)->with('error', 'Claim failed because you have claimed this product.');
     }
 
 
@@ -98,16 +106,16 @@ Route::post('/claim', function(Request $request) {
     auth()->user()->buying_limit -= 1;
     auth()->user()->save();
 
-    return redirect(route('user.index').'/#claimNo'.$request->id)->with('success', 'Claim success');
+    return redirect(route('user.index') . '/#claimNo' . $request->id)->with('success', 'Claim success');
 })->name('user.claim')->middleware(isLogin::class);
 
-Route::get('/{id}/detail', function($id){
+Route::get('/{id}/detail', function ($id) {
     $productDepartement = ProductDepartement::findOrFail($id);
     $transactions = Transaction::where('product_departement_id', $productDepartement->id)->get();
     return view('product_departement.detail', ['productDepartement' => $productDepartement, 'transactions' => $transactions]);
 })->name('product_departement.detail')->middleware(isLogin::class);
 
-Route::get('/class', function(){
+Route::get('/class', function () {
     $departement = auth()->user()->departement;
     return view('user.departement', ['departement' => $departement]);
 })->name('departement.detail')->middleware(isLogin::class);
@@ -115,39 +123,38 @@ Route::get('/class', function(){
 
 
 
-Route::get('/search', function(){
+Route::get('/search', function () {
     return redirect()->back()->with('error', 'Search feature is not available yet.');
 })->name('user.search')->middleware(isLogin::class);
 
-Route::get('/claimed', function(){
+Route::get('/claimed', function () {
     $transactions = Transaction::where('user_id', auth()->id())->get();
     return view('user.claimed', ['transactions' => $transactions]);
 })->name('user.info')->middleware(isLogin::class);
 
-Route::prefix('admin')->group(function() {
-    Route::get('/', function(Request $request){
+Route::prefix('admin')->group(function () {
+    Route::get('/', function (Request $request) {
         $users = User::all();
         if ($request->has('departement') && $request->departement != '') {
             $users = User::where('departement_id', $request->departement)->get();
         }
         // sort by departement name then by user name
-        $users = $users->sortBy(function($user){
+        $users = $users->sortBy(function ($user) {
             return $user->departement->name . $user->name;
         });
         return view('admin.index', ['users' => $users]);
     })->name('admin.index')->middleware([isLogin::class, isAdmin::class]);
 
-    Route::get('/env', function(){
+    Route::get('/env', function () {
         return view('admin.edit_env_variables');
     })->name('admin.edit_env_variables')->middleware([isLogin::class, isAdmin::class])->name('admin.edit_env_variables');
 
-    Route::post('/env', function(Request $request){
+    Route::post('/env', function (Request $request) {
         $WebVariables = WebVariable::all();
-        foreach($WebVariables as $WebVariable){
+        foreach ($WebVariables as $WebVariable) {
             $WebVariable->value = $request[$WebVariable->name];
             $WebVariable->save();
         }
         return redirect()->back();
     })->middleware([isLogin::class, isAdmin::class]);
 });
-
